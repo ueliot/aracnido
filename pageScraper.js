bd91390bf8e92e03e11723d52e84456d51404327
Eliot Uriarte <ueliot@yahoo.com>
2020-12-19 14:46:11 +0100

    add files base using puppeteer

---
+++ pageScraper.js
@@ -0,0 +1,12 @@
+const scraperObject = {
+    url: 'http://books.toscrape.com',
+    async scraper(browser){
+        let page = await browser.newPage();
+        console.log(`Navigating to ${this.url}...`);
+        await page.goto(this.url);
+       
+
+    }
+}
+
+module.exports = scraperObject;
