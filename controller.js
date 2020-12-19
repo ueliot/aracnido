93c9a59e22c1f8b38f75296fcd57ae37c6a43950
Eliot Uriarte <ueliot@yahoo.com>
2020-12-19 23:22:42 +0100

    scrap with categories

--- controller.js
+++ controller.js
@@ -3,8 +3,11 @@ async function scrapeAll(browserInstance){
     let browser;
     try{
         browser = await browserInstance;
-        await pageScraper.scraper(browser);
-
+        let scrapedData = {};
+        // Call the scraper for different set of books to be scraped
+        scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
+        await browser.close();
+        console.log(scrapedData)
     }
     catch(err){
         console.log("Could not resolve the browser instance => ", err);
